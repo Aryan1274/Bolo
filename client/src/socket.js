ea9fd6e24be75f40240aed32b70b91f385f850ed
Aryan1274 <aryan407012@gmail.com>
2025-07-26 22:58:02 +0530

    Update: made changes to backend/frontend code

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // backend server URL
+const socket = io("https://bolo-6by1.onrender.com"); // backend server URL
 
 export default socket;
